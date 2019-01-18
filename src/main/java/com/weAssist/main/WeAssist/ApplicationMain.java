5564f9d46fcab28e2cf6a0bc14f6af55c22389ea
mrgurtejsingh <gurtej.nitj@gmail.com>
2019-01-19 01:20:57 +0530

    For db connecting using jpa mysql

--- src/main/java/com/weAssist/main/WeAssist/ApplicationMain.java
+++ src/main/java/com/weAssist/main/WeAssist/ApplicationMain.java
@@ -2,18 +2,12 @@ package com.weAssist.main.WeAssist;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * This is the main Start up application for
  */
 @SpringBootApplication
-@ComponentScan("weAssist.components.*")
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-
 public class ApplicationMain {
 
     /**
