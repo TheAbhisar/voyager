5e1b116434d665a8b68604b36727a6dc0eecb55d
mrgurtejsingh <gurtej.nitj@gmail.com>
2019-01-19 10:08:39 +0530

    For services

--- src/main/java/com/weAssist/service/FindAssistanceService.java
+++ src/main/java/com/weAssist/service/FindAssistanceService.java
@@ -1,9 +1,5 @@
 package com.weAssist.service;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-
 import org.springframework.stereotype.Service;
 
 import com.google.gson.Gson;
@@ -12,29 +8,22 @@ import com.google.gson.JsonParser;
 import com.weAssist.model.LocationResponse;
 
 @Service
-public class GeoLocationService {
+public class FindAssistanceService {
 	
 	
-	public LocationResponse getGeoLocation(){		
+	public LocationResponse getAssistance(){		
 		
 		Gson gson = new Gson();
 		JsonParser parser = new JsonParser();		
-		
-		Client client = ClientBuilder.newClient();
-		WebTarget base = client.target("https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY");
-		WebTarget postUrl = base.path("add");
-		//Response response = postUrl.request(MediaType.APPLICATION_JSON).post(Entity.json(""));
-		
+				
 		String responseString = "{'location': {'lat': '51.0','lng': '-0.1'},'accuracy': '1200.4'}";
 		
 		JsonObject object = (JsonObject) parser.parse(responseString); // response will be the json String
 		LocationResponse locResp = gson.fromJson(object, LocationResponse.class); 		
         
-	    client.close();	
+	    	
 	    
 	    return locResp;		
 	}
-	
-	
 
 }
